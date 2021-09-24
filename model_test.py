2598c102ebef4038086beefb01161c0e43bba586
yuko29 <yuko29.cs07@nctu.edu.tw>
2021-09-25 00:31:16 +0800

    change onnx ir_version to 3

--- model_test.py
+++ model_test.py
@@ -2,19 +2,34 @@ import onnx
 import onnxruntime as ort
 import numpy as np
 from onnx import numpy_helper
+from onnx import version_converter
+import configparser
 
-
+config = configparser.ConfigParser()
+config.read('setting.ini')
 
 # Load the ONNX model
-model = onnx.load("./MaxPool/single_maxpool.onnx")
+model = onnx.load(config['set']['model'])
 
 # Check that the model is well formed
 onnx.checker.check_model(model)
 
+# lower the onnx ir_virsion (ONNC available in  ONNX v1.3 / ir_version 3)
+target_version = 8
+ir_version     = 3
+
+model_path     = config['set']['model']
+original_model = onnx.load(model_path)
+
+converted_model = version_converter.convert_version(original_model , target_version)
+converted_model.ir_version = ir_version
+
+onnx.save(converted_model, model_path)
+
 # Print a human readable representation of the graph
 print(onnx.helper.printable_graph(model.graph))
 
-ort_session = ort.InferenceSession("./MaxPool/single_maxpool.onnx")
+ort_session = ort.InferenceSession(config['set']['model'])
 
 np_array = np.random.randn(1, 1, 32, 32).astype(np.float32)
 
@@ -27,10 +42,10 @@ outputs = ort_session.run(
 # convert np array to TensorProto
 intput_tensor = numpy_helper.from_array(np_array)
 # save TensorProto
-with open('./MaxPool/maxpool_input.pb', 'wb') as f:
+with open(config['set']['save_input'], 'wb') as f:
     f.write(intput_tensor.SerializeToString())
 
 output_tensor = np.array(outputs)
 output_tensor = numpy_helper.from_array(output_tensor)
-with open('./MaxPool/maxpool_output.pb', 'wb') as f:
+with open(config['set']['save_output'], 'wb') as f:
     f.write(output_tensor.SerializeToString())
